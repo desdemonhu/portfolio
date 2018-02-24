a7739f3d224f4cbcb7d83430efb2c1dce925d9d7
Raegan Millhollin <desdemonhu@gmail.com>
2018-02-23 16:03:16 -0800

    Added Featured project

--- src/component/featured/index.js
+++ src/component/featured/index.js
@@ -4,14 +4,33 @@ import ReactDOM from 'react-dom';
 class Home extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            projectIndex: 0
+        }
     }
+
+    advanceThroughProjects = () => {
+        let newIndex = this.state.projectIndex + 1;
+        if(this.state.projectIndex < this.props.projects.length -1) {
+            this.setState({projectIndex: newIndex})
+        }else {
+            this.setState({projectIndex: 0})
+            }
+        }
 
     render() {
         return (
-            <div>
-                <h3>Title: {this.props.projects[0].name}</h3>
-                <p>Description: 
-                </p>
+            <div id="featured-section" className="clearfix">
+            <div className="featured-project-image">
+                <img src={this.props.projects[this.state.projectIndex].image} />
+            </div>
+                <div className="featured-project-text">
+                    <h3><a href={this.props.projects[this.state.projectIndex].url}>{this.props.projects[this.state.projectIndex].name}</a></h3>
+                    <p>{this.props.projects[this.state.projectIndex].description}</p> 
+                </div> 
+                <div className="featured-project-button">
+                    <button onClick={this.advanceThroughProjects}> > </button>
+                </div>
             </div>
         )
     }
--- src/reducers/feature-reducer.js
+++ src/reducers/feature-reducer.js
@@ -3,11 +3,24 @@ const json = require('../../data/projects.json');
 
 let initState = [];
 
+const getNewestProject = function () {
+    initState.sort((a, b) => {
+        const aPublished = new Date(a.published);
+        const bPublished = new Date(b.published)
+        if (aPublished > bPublished) return -1;
+        if (aPublished < bPublished) return 1;
+        return 0;
+    })
+}
+
 const fetchData = function(){
     json.map(project => {
         initState.push(new Project(project))
     })
+    getNewestProject();
 }();
+
+
 
 const reducer = (state = initState, action) => {
   let { type, payload } = action;
