66cd674209b2c44f9e77a1808903b9ffade9dd57
Raegan Millhollin <desdemonhu@gmail.com>
2018-02-26 12:27:26 -0800

    Added get for hopefully React routing

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 4000;
 
 app.use(express.static(`${__dirname}/build`));
 
-// app.get('/*', function (there, backAgain) {
-//   backAgain.sendFile('index.html', { root: './build' });
-// });
+app.get('/*', function (there, backAgain) {
+  backAgain.sendFile('index.html', { root: './build' });
+});
 
 app.listen(PORT, function () {
   // lets you know which port your server has started on
