45cdc0b7bf2e7c319887f6934183da2ec54228ec
Raegan Millhollin <desdemonhu@gmail.com>
2018-02-26 12:52:45 -0800

    added file loader

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,12 @@ module.exports = {
           }
         ),
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
     ],
   },
 };
